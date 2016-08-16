2b6ed52119afa2f5b71340bf310238d21130bb97
Daniel A. Maierhofer <git@damadmai.at>
2016-08-16 18:56:05 +0200

    Add RFID

--- MOM-Glove/MOM-Glove.cpp
+++ MOM-Glove/MOM-Glove.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 #include "Adafruit_BNO055.h"
 #include "Adafruit_DRV2605.h"
+
 extern "C" {
 	//#include "pfleury/uart.h"
 };
@@ -19,9 +21,16 @@ LOCKBITS = LB_MODE_3;	//0xFC
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 Adafruit_DRV2605 drv = Adafruit_DRV2605();
 
+SoftwareSerial SoftSerial(2, 3);
+unsigned char buffer[64];
+uint8_t ledcnt = 0;
+
+
 void setup()
 {
+	pinMode(13, OUTPUT);
 	Serial.begin(115200);
+	SoftSerial.begin(9600);
 	if(!bno.begin())
 	{
 		//uart_puts("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
@@ -38,13 +47,54 @@ void setup()
 
 void loop()
 {
-	sensors_event_t event;
-	bno.getEvent(&event);
+	//sensors_event_t event;
+	//bno.getEvent(&event);
 
-	Serial.print(event.orientation.x, 4);
-	Serial.print(";");
-	Serial.print(event.orientation.y, 4);
-	Serial.print(";");
-	Serial.println(event.orientation.z, 4);
+	//Serial.print(event.orientation.x, 4);
+	//Serial.print(";");
+	//Serial.print(event.orientation.y, 4);
+	//Serial.print(";");
+	//Serial.println(event.orientation.z, 4);
+
+	if (ledcnt < 10)
+	{
+		if (ledcnt == 0)
+		{
+			digitalWrite(13, HIGH);
+		}
+		ledcnt++;
+	}
+	else
+	{
+		digitalWrite(13, LOW);
+	}
+
 	delay(10);
+
+	if (SoftSerial.available())
+	{
+		static int count = 0;
+		while(SoftSerial.available())
+		{
+			uint8_t c = SoftSerial.read();
+			if (c == 0x02)
+			{
+				count = 0;
+			}
+			else if (c == 0x03)
+			{
+				Serial.write(buffer,count);
+				Serial.write('\n');
+				ledcnt = 0;
+			}
+			else
+			{
+				buffer[count++]=c;
+			}
+			if(count == 64)
+				break;
+		}
+	}
+	if (Serial.available())
+		SoftSerial.write(Serial.read());
 }
